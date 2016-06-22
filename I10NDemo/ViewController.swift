3501520310ccb004e83942f2325011e0d1daecc7
丁暐哲 <peter@dingweizhedeMBP.Goodideas-studio>
2016-06-22 18:37:04 +0800

    simple demo

--- I10NDemo/ViewController.swift
+++ I10NDemo/ViewController.swift
@@ -8,18 +8,64 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UITextFieldDelegate {
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var currencyLabel: UILabel!
+    @IBOutlet weak var weightLabel: UILabel!
+    @IBOutlet weak var BMIlabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var firstTextField: UITextField!{
+        didSet{
+                firstTextField.delegate = self
+        }
+    }
+    
+    @IBOutlet weak var sencondTextfield: UITextField!{
+        didSet{
+            sencondTextfield.delegate = self
+        }
+    }
 
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    let currency = NSNumberFormatter()  //
+    let weight = NSMassFormatter()
+   
+    let now = NSDate()
+    let timeFormatter = NSDateFormatter()
+    
+    
+    @IBAction func clickButton(sender: UIButton) {
+        weight.unitStyle = .Long
+        weightLabel.text = weight.stringFromKilograms(Double(firstTextField.text!)!)
+          let height = String.localizedStringWithFormat("%.3f", Double(firstTextField.text!)!/(1.732*1.732))
+        BMIlabel.text = height
+        
+        let currencyString = currency.stringFromNumber(Int(sencondTextfield.text!)!)
+        
+        currencyLabel.text =  currencyString
+//        timeFormatter.setLocalizedDateFormatFromTemplate("yyyyMMddjjmmss")
+//        timeFormatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
+        timeFormatter.dateStyle = .ShortStyle
+        timeFormatter.timeStyle = .ShortStyle
+        let date = timeFormatter.stringFromDate(now)
+        dateLabel.text = date
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        currency.numberStyle = NSNumberFormatterStyle.CurrencyISOCodeStyle
+        
+      
+        
+        let currencyString = currency.stringFromNumber(200)
+        nameLabel.text = "DinDin"
+        currencyLabel.text =  currencyString
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 
 }
 
